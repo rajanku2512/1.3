b0ebc6c161514d2fe157c0f27f45ff519cba1449
rajanku2512 <rajanku2512@gmail.com>
2016-12-23 02:15:23 +0530

    Add files via upload

---
+++ Assignment1.3.java
@@ -0,0 +1,11 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10, y=20;
+System.out.println("value of y="+x);
+System.out.println("value of x="+y);
+	}
+
+}
